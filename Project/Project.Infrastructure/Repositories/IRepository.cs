bbb6778a00de117d10f1f2efd3e8facf605ed27e
yanqilu <18504029848@163.com>
2020-11-27 14:37:42 +0800

    仓储

--- Project/Project.Infrastructure/Repositories/IRepository.cs
+++ Project/Project.Infrastructure/Repositories/IRepository.cs
@@ -27,8 +27,19 @@ namespace Project.Infrastructure.Repositories
     /// <summary>
     /// Repository
     /// </summary>
-    public interface IRepository<T> where T : Entity
+    public interface IRepository<TEntity> where TEntity : Entity
     {
-       
+        IQueryable<TEntity> Table { get; }
+        IQueryable<TEntity> TableNoTracking { get; }
+
+        Task DeleteAsync(TEntity entity);
+        Task DeleteByIdAsync(object id);
+        Task DeleteEnumerableAsync(IEnumerable<TEntity> entities);
+        Task DeleteSoftByIdAsync(object id);
+        Task<TEntity> GetByIdAsync(object id);
+        Task InsertAsync(TEntity entity);
+        Task InsertEnumerableAsync(IEnumerable<TEntity> entities);
+        Task UpdateAsync(TEntity entity);
+        Task UpdateEnumerableAsync(IEnumerable<TEntity> entities);
     }
 }
