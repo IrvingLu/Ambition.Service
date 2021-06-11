9db8bc87eb59c7f6bd9d1e7027a2ec8277942c24
seven <18504029848@163.com>
2021-06-11 13:26:28 +0800

    添加lambada条件拼接类，去除无用的文件夹

--- Identity/Identity.Core/Domain/ApplicationRole.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Identity.Core.Domain
-{
-    public class ApplicationRole : IdentityRole
-    {
-    }
-}
--- Identity/Identity.Core/Domain/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Identity.Core.Domain
-{
-    public class ApplicationUser : IdentityUser
-    {
-
-    }
-}
